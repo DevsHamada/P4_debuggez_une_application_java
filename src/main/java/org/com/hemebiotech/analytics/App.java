09be8ebbeedab80288910f2ffd408ab99d3aaf71
Mohamed ZEJLI <mohamed.zejli@gmail.com>
2022-02-18 15:37:54 +0100

    Initial commit

---
+++ src/main/java/org/com/hemebiotech/analytics/App.java
@@ -0,0 +1,13 @@
+package org.com.hemebiotech.analytics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from here!" );
+    }
+}
