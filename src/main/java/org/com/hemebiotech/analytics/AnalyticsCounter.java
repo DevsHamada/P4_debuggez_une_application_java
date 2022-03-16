5845b5c331472f99bacf31cdc5085fec32322836
Mohamed ZEJLI <mohamed.zejli@gmail.com>
2022-03-16 11:39:36 +0100

    code review for soutenance

--- src/main/java/org/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/main/java/org/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,12 +7,12 @@ public class AnalyticsCounter implements IAnalyticsCounter {
     @Override
     public Map<String, Integer> analyticsCount(List<String> list) {
         Map<String, Integer> listSymptoms = new TreeMap<>();
-        for (int i = 0; i < list.size(); i++) {
-            int Frequencies = Collections.frequency(list, list.get(i));
-            if (listSymptoms.containsKey(list.get(i))) {
-                listSymptoms.put(list.get(i), Frequencies);
+        for (String symptoms : list) {
+            int frequencies = Collections.frequency(list, symptoms);
+            if (listSymptoms.containsKey(symptoms)) {
+                listSymptoms.put(symptoms, frequencies);
             } else {
-                listSymptoms.put(list.get(i), 1);
+                listSymptoms.put(symptoms, 1);
             }
         }
         return listSymptoms;
